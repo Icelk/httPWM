efc7f8759000f3c67f4c55892ea3ad28e7920b4e
Icelk <main@icelk.dev>
2021-03-04 20:05:20 +0100

    Added support for software PWM

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,6 +2,13 @@ use pwm_dev::*;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
+    #[cfg(not(feature = "test"))]
+    // let pwm = rppal::pwm::Pwm::new(rppal::pwm::Channel::Pwm0).expect("failed to get PWM");
+    let pwm = rppal::gpio::Gpio::new()
+        .expect("failed to get GPIO")
+        .get(19)
+        .expect("failed to get pin")
+        .into_output();
     let in_three_minutes = chrono::Local::now().time() + chrono::Duration::minutes(3);
     let transition = Transition {
         from: Strength::new(0.0),
