77e92b16ba268cd349dadf263f00dfde152fc51c
Icelk <main@icelk.dev>
2021-03-13 13:57:29 +0100

    Added /add-scheduler logic in server This means the user now can insert schedulers!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -144,11 +144,36 @@ fn create_server<T: VariableOut + Send>(controller: Arc<Mutex<Controller<T>>>) -
         (utility::ContentType::JSON, Cached::Dynamic)
     });
 
+    // todo!("Use chrono::Native");
+    let controller = ctl();
+    bindings.bind_page("/add-scheduler", move |buffer, req, cache| {
+        let command = serde_json::from_slice(req.body())
+            .ok()
+            .and_then(|data: AddSchedulerData| data.to_command());
+
+        match command {
+            Some(command) => controller.lock().unwrap().send(command),
+            None => {
+                utility::write_error(buffer, 400, cache);
+            }
+        }
+
+        (utility::ContentType::Html, Cached::Dynamic)
+    });
+
     let localhost = Host::no_certification("web", Some(bindings));
     let hosts = HostData::builder(localhost).build();
     let ports = vec![(8080, ConnectionSecurity::http1(), hosts)];
 
-    Config::new(ports)
+    let mut config = Config::new(ports);
+    config.mount_extension(kvarn_extensions::cache);
+    config
+}
+
+pub fn parse_time(string: &str) -> Option<chrono::NaiveTime> {
+    chrono::NaiveTime::parse_from_str(string, "%H:%M:%S")
+        .or_else(|_| chrono::NaiveTime::parse_from_str(string, "%H:%M"))
+        .ok()
 }
 
 #[derive(Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -160,11 +185,7 @@ impl DayData {
     pub fn to_command(self) -> Option<Command> {
         let day: chrono::Weekday = self.day.parse().ok()?;
         let time = match self.time {
-            Some(time) => Some(
-                chrono::NaiveTime::parse_from_str(time.as_str(), "%H:%M:%S")
-                    .or_else(|_| chrono::NaiveTime::parse_from_str(time.as_str(), "%H:%M"))
-                    .ok()?,
-            ),
+            Some(time) => Some(parse_time(&time)?),
             None => None,
         };
         Some(Command::ChangeDayTimer(day, time))
@@ -235,5 +256,210 @@ impl StateData {
             days,
             transition: TransitionData::from_transition(&state.get_week_schedule().transition),
         }
+    }
+}
+pub mod extra_schedulers {
+    use chrono::Datelike;
+    use pwm_dev::scheduler::Keep;
+
+    use super::*;
+
+    pub(crate) fn get_next_day<F: Fn(chrono::Weekday) -> Option<chrono::NaiveTime>>(
+        from: chrono::Weekday,
+        get: F,
+    ) -> Option<(chrono::NaiveTime, u8)> {
+        let mut day = from;
+
+        for passed in 0..7 {
+            day = day.succ();
+            let time = get(day);
+            if time.is_some() {
+                return time.map(|t| (t, passed + 1));
+            }
+        }
+        None
+    }
+    pub(crate) fn apply_day_offset(
+        date_time: chrono::DateTime<chrono::Local>,
+        offset: i64,
+    ) -> chrono::DateTime<chrono::Local> {
+        date_time + chrono::Duration::days(offset)
+    }
+
+    #[derive(Debug)]
+    pub struct Common {
+        description: String,
+        command: Command,
+    }
+    impl Common {
+        /// Returns `Err` when command is not clonable
+        pub fn new(description: String, command: Command) -> Result<Self, ()> {
+            if !command.can_clone() {
+                Err(())
+            } else {
+                Ok(Self {
+                    description,
+                    command,
+                })
+            }
+        }
+        pub fn get_command(&self) -> Command {
+            // Ok, since it's guaranteed the command in `Common` is clonable.
+            self.command.try_clone().unwrap()
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct At {
+        common: Common,
+        moment: chrono::NaiveDateTime,
+    }
+    impl At {
+        pub fn new(common: Common, moment: chrono::NaiveDateTime) -> Self {
+            Self { common, moment }
+        }
+    }
+    impl Scheduler for At {
+        fn get_next(&self) -> Option<(Duration, Command)> {
+            let now = chrono::Local::now();
+            let fixed_now = now.date().naive_local().and_time(now.time());
+            match (self.moment - fixed_now).to_std() {
+                Ok(dur) => Some((dur, Command::clone(&self.common.command))),
+                Err(_) => None,
+            }
+        }
+        fn advance(&mut self) -> Keep {
+            Keep::Remove
+        }
+        fn description(&self) -> &str {
+            self.common.description.as_str()
+        }
+        fn kind(&self) -> Option<&str> {
+            Some("AtDateTime")
+        }
+    }
+    #[derive(Debug)]
+    pub struct EveryWeek {
+        common: Common,
+        time: chrono::NaiveTime,
+        day: chrono::Weekday,
+    }
+    impl EveryWeek {
+        pub fn new(common: Common, time: chrono::NaiveTime, day: chrono::Weekday) -> Self {
+            Self { common, time, day }
+        }
+    }
+    impl Scheduler for EveryWeek {
+        fn get_next(&self) -> Option<(Duration, Command)> {
+            let now = chrono::Local::now();
+            if self.day == now.weekday() && now.time() < self.time {
+                // Unwrap is OK, now will never be over self.time.
+                Some((
+                    (self.time - now.time()).to_std().unwrap(),
+                    self.common.get_command(),
+                ))
+            } else {
+                get_next_day(now.weekday(), |day| {
+                    if day == self.day {
+                        Some(self.time)
+                    } else {
+                        None
+                    }
+                })
+                .map(|(time, offset)| {
+                    // unwrap is OK, since date is always `.succ()`
+                    (apply_day_offset(
+                        now.date()
+                            .and_time(time)
+                            .expect("invalid DateTime in EveryWeek scheduler"),
+                        offset as i64,
+                    ) - now)
+                        .to_std()
+                        .unwrap()
+                })
+                .map(|dur| (dur, self.common.get_command()))
+            }
+        }
+        fn advance(&mut self) -> Keep {
+            Keep::Keep
+        }
+        fn description(&self) -> &str {
+            self.common.description.as_str()
+        }
+        fn kind(&self) -> Option<&str> {
+            Some("EveryWeekAt")
+        }
+    }
+    #[derive(Debug)]
+    pub struct EveryDay {
+        common: Common,
+        time: chrono::NaiveTime,
+    }
+    impl EveryDay {
+        pub fn new(common: Common, time: chrono::NaiveTime) -> Self {
+            Self { common, time }
+        }
+    }
+    impl Scheduler for EveryDay {
+        fn get_next(&self) -> Option<(Duration, Command)> {
+            let now = chrono::Local::now();
+            Some(if now.time() < self.time {
+                // Unwrap is OK, now will never be over self.time.
+                (
+                    (self.time - now.time()).to_std().unwrap(),
+                    self.common.get_command(),
+                )
+            } else {
+                // Unwrap is OK, it's one day ahead!
+                (
+                    ((self.time - now.time()) + chrono::Duration::days(1))
+                        .to_std()
+                        .unwrap(),
+                    self.common.get_command(),
+                )
+            })
+        }
+        fn advance(&mut self) -> Keep {
+            Keep::Keep
+        }
+        fn description(&self) -> &str {
+            self.common.description.as_str()
+        }
+        fn kind(&self) -> Option<&str> {
+            Some("EveryDayAt")
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct AddSchedulerData {
+    kind: String,
+    time: String,
+    name: String,
+    description: String,
+    extras: Vec<String>,
+    transition: TransitionData,
+}
+impl AddSchedulerData {
+    pub fn to_command(self) -> Option<Command> {
+        let transition = self.transition.to_transition()?;
+        let time = parse_time(&self.time)?;
+        let run_command = Command::SetTransition(transition);
+        let common = extra_schedulers::Common::new(self.description, run_command).unwrap();
+
+        let scheduler: Arc<Mutex<dyn Scheduler>> = match self.kind.as_str() {
+            "at" if self.extras.len() == 1 => Arc::new(Mutex::new(extra_schedulers::At::new(
+                common,
+                chrono::NaiveDate::parse_from_str(self.extras[0].as_str(), "%Y-%m-%d")
+                    .ok()?
+                    .and_time(time),
+            ))),
+            "every-week" if self.extras.len() == 1 => Arc::new(Mutex::new(
+                extra_schedulers::EveryWeek::new(common, time, self.extras[0].parse().ok()?),
+            )),
+            "every-day" => Arc::new(Mutex::new(extra_schedulers::EveryDay::new(common, time))),
+            _ => return None,
+        };
+        Some(Command::AddReplaceScheduler(self.name, scheduler))
     }
 }
